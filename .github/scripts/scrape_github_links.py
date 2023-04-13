9f5a8def6dd55cb8e5fb2b7cb91e08d66a1a567a
DonnieBLT <128622481+DonnieBLT@users.noreply.github.com>
2023-04-13 17:27:05 -0400

    Create scrape_github_links.py

---
+++ .github/scripts/scrape_github_links.py
@@ -0,0 +1,28 @@
+import json
+import re
+import requests
+from bs4 import BeautifulSoup
+
+def extract_github_links(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    links = soup.find_all('a', href=True)
+    github_links = [link['href'] for link in links if 'github.com' in link['href']]
+    return github_links
+
+with open('www_project_repos.json', 'r') as f:
+    data = json.load(f)
+
+project_links = []
+
+for project in data:
+    project_name = project['name']
+    github_url = project['html_url'].replace('github.com', 'github.com/OWASP')
+    repo_links = extract_github_links(github_url)
+    project_links.append({
+        'project_name': project_name,
+        'repo_links': repo_links
+    })
+
+with open('project_repos_links.json', 'w') as f:
+    json.dump(project_links, f, indent=2)
