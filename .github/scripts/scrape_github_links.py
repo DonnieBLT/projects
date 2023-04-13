e92f6bd34d2fbdcecd17c3bc7b0e4b938a6af218
DonnieBLT <128622481+DonnieBLT@users.noreply.github.com>
2023-04-13 19:22:27 -0400

    Update scrape_github_links.py

--- .github/scripts/scrape_github_links.py
+++ .github/scripts/scrape_github_links.py
@@ -3,7 +3,7 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-def extract_github_links(url):
+def extract_github_links(url, project_name):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     links = soup.find_all('a', href=True)
@@ -11,7 +11,7 @@ def extract_github_links(url):
     github_links = set()
     for link in links:
         match = re.match(r'https://github\.com/[^/]+/[^/#]+', link['href'].lower())
-        if match:
+        if match and project_name.lower() not in link['href'].lower():
             github_links.add(match.group(0))
     
     return list(github_links)
@@ -26,11 +26,12 @@ for project in data:
     project_name = project['name']
     github_url = project['html_url'].replace('github.com/OWASP/', 'owasp.org/')
 
-    repo_links = extract_github_links(github_url)
-    project_links.append({
-        'project_name': project_name,
-        'repo_links': repo_links
-    })
+    repo_links = extract_github_links(github_url, project_name)
+    if repo_links:  # Only append projects with GitHub links
+        project_links.append({
+            'project_name': project_name,
+            'repo_links': repo_links
+        })
 
 with open('project_repos_links.json', 'w') as f:
     json.dump(project_links, f, indent=2)
